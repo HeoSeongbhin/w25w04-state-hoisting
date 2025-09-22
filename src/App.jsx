4bf2b678eee4856af8a8f9011d4956e63a233641
HeoSeongbhin <hsbb1266@gmail.com>
2025-09-22 15:32:54 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button
+        onClick={() => setCount(prev => prev + 1)}>
+        증가
+      </button>
+    </div>
+  )
+}
+
+export default App
+
+//각 역할, 하는 일
